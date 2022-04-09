6b34f617d7961055cae8af3c475e852a0bbc6402
Taras Ozarko <ivanovych666@gmail.com>
2022-04-09 14:09:25 +0300

    update jsonSort options

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,32 @@ import {JSONWriter} from './JSONWriter';
 import {Comparator, Compare} from './Comparator';
 import {Comparators} from './comparators';
 
-export const jsonSort = (input: string, compare: Compare<string> = Comparators.alphabeticalSort): string => {
+interface JsonSortOptions {
+    caseInsensitive: boolean;
+    reverse: boolean;
+    natural: boolean;
+}
+
+export const jsonSort = (input: string, options?: JsonSortOptions | Compare<string>): string => {
+    let compare: Compare<string> | null = null;
+
+    if (typeof options === 'function') {
+        compare = options;
+    } else if (options) {
+        const {caseInsensitive, reverse, natural} = options;
+        const comparatorName = [
+            natural ? 'natural' : 'alphabetical',
+            reverse ? 'Reverse' : '',
+            caseInsensitive ? 'Ci' : '',
+            'Sort',
+        ].join('');
+        compare = (Comparators as any)[comparatorName];
+    }
+
+    if (!compare) {
+        compare = Comparators.alphabeticalSort;
+    }
+
     const comparator = new Comparator(compare);
     const writer = new JSONWriter(comparator);
     const reader = new JSONReader(input);
